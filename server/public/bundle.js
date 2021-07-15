3284354813a0ce7c50afe9035dcf178728e08e8e
dave griffiths <dave@fo.am>
2021-07-15 14:27:37 +0100

    tweaks

--- server/public/bundle.js
+++ server/public/bundle.js
@@ -24317,7 +24317,6 @@ function redraw_graph(graph_data,scale) {
 	
 	for (let year of graph_data) {
 		let dec = Math.floor((year.year%2000)/10);
-		console.log(dec);
 		if (decades[dec]==undefined) {
 			decades[dec]=[year.avg]
 		} else {
@@ -24325,14 +24324,10 @@ function redraw_graph(graph_data,scale) {
 		}
 	}
 
-	console.log(decades);
-
 	for (let dec of Object.keys(decades)) {
 		decades[dec]=arr2avg(decades[dec])
 	}
 	
-	console.log(decades);
-
 	let minimum = 999999;
 	let maximum = 0;
 
@@ -24435,15 +24430,19 @@ function update_lsoa() {
 
 						  if (lsoa_zones_include(feature.properties.name,lsoa_zones)) {
 							  layer.setStyle({'color': highlight_col});
+							  layer.setStyle({'fillOpacity': 0.5});
 						  } else {
 							  layer.setStyle({'color': zone_col})
+							  layer.setStyle({'fillOpacity': 0.02});
 						  }
 
 						  layer.setStyle({'weight': 1});
+						  layer.setStyle({'opacity': 1});
 
 						  layer.on('click', function(e) {
  							  if (!lsoa_zones_include(feature.properties.name,lsoa_zones)) {
 								  layer.setStyle({'color': highlight_col});
+								  layer.setStyle({'fillOpacity': 0.5});
 								  lsoa_zones.push({
 									  name: feature.properties.name,
 									  tile: feature.properties.zone
@@ -24451,6 +24450,7 @@ function update_lsoa() {
 							  } else {
 								  layer.setStyle({'color': zone_col});
 								  lsoa_zones=remove_lsoa_zone(feature.properties.name,lsoa_zones);
+								  layer.setStyle({'fillOpacity': 0.02});
 							  }
 							  update_lsoa_list();
 						  });
