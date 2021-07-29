6bf006c618b6a1500e52578bac3596a87e750486
dave griffiths <dave@fo.am>
2021-07-29 10:31:58 +0100

    added test network

--- server/public/src/adaptation_finder.js
+++ server/public/src/adaptation_finder.js
@@ -1,48 +1,30 @@
 const $ = require("jquery")
 
-class Impact {
-	constructor (type,description,references) {
-		this.type=type
-		this.description=description
-		this.references=references
+// two values for a specific variable to use for comparisons
+class ClimateVariable {
+	constructor (variable_name,reference,value) {
+		this.variable_name=variable_name
+		this.reference=reference
+		this.value=value
 	}
 }
 
-class Adaptation {
-	constructor (description,examples) {
-		this.description=description
-		this.examples=examples		
-	}
-}
-
-// operators:
-//
-//   increase
-//   decrease
-//   less-than
-//   greater-than
-//   ...
-
-class Trend {
-	constructor (variable_name,operator,threshold,impacts,adaptations,sector,subsector,trend,priority) {
+// a cause is a trigger that can be set off by different variables
+class Cause {
+	constructor (variable_name,operator,threshold) {
 		this.variable_name=variable_name
 		this.operator=operator
 		this.threshold=threshold
-
-		this.impacts=impacts
-		this.adaptations=adaptations
-
-		// metadata
-		this.sector=sector
-		this.subsector=subsector
-		this.trend=trend
-		this.priority=priority
+		switch(variable_name) {
+		case "mean_windspeed": this.image="image/wind.svg"
+		case "mean_temperature": this.image="image/temp.svg"
+		case "daily_precip": this.image="image/rain.svg"
+		default: console.log("no svg for "+variable_name)
+		}
 	}
 
 	isActive(variable) {
-		if (variable.variable_name==this.variable_name) {
-			console.log(variable)
-			console.log(this.operator)
+		if (variable.variable_name==this.variable_name) {			
 			switch (this.operator) {
 			case "increase":
 				if (variable.reference<variable.value)
@@ -68,14 +50,45 @@ class Trend {
 		}
 		return false
 	}
+
 }
 
-class ClimateVariable {
-	constructor (variable_name,reference,value) {
-		this.variable_name=variable_name
-		this.reference=reference
-		this.value=value
+class Impact {
+	constructor (type,description,references) {
+		this.type=type
+		this.description=description
+		this.references=references
 	}
+}
+
+class Adaptation {
+	constructor (description,examples) {
+		this.description=description
+		this.examples=examples		
+	}
+}
+
+// operators:
+//
+//   increase
+//   decrease
+//   less-than
+//   greater-than
+//   ...
+
+class Trend {
+	constructor (cause,impacts,adaptations,sector,subsector,trend,priority) {
+		this.cause=cause		
+		this.impacts=impacts
+		this.adaptations=adaptations
+
+		// metadata
+		this.sector=sector
+		this.subsector=subsector
+		this.trend=trend
+		this.priority=priority
+	}
+
 }
 
 class AdaptationFinder {
@@ -127,17 +140,18 @@ class AdaptationFinder {
 		let active_trends = []
 		for (let trend of this.trends) {
 			for (let variable of this.variables) {
-				if (trend.isActive(variable)) {
+				if (trend.cause.isActive(variable)) {
+					console.log(variable.variable_name+" is "+cause.operator)
 					active_trends.push(trend)
 				}
-			}			
+			}
 		}
 		return active_trends
 	}
 }
 
 const the_trends = [
-	new Trend("mean_windspeed", "increase", 0,
+	new Trend(new Cause("mean_windspeed", "increase", 0),
 				[new Impact("Sector",
 							"Increased wind speed leads to decreased cycling. More people use public transport networks.",
 							["https://dx.doi.org/10.1186/1476-069x-11-12"]),
@@ -149,7 +163,7 @@ const the_trends = [
 				"Active Transport",
 				"Rising",
 				"High"),
-	new Trend("daily_precip", "increase", 0,
+	new Trend(new Cause("daily_precip", "increase", 0),
 				[new Impact("Sector",
 							"Increased precipitation leads to decreased cycling.",
 							["https://dx.doi.org/10.1186/1476-069x-11-12",
