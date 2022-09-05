3c760df5063143650cfcc642fe6dec8ff0137598
Dave Griffiths <dave@thentrythis.org>
2022-09-05 08:29:40 +0100

    new crime icon fixed: #76

--- data/builder/chessscape.py
+++ data/builder/chessscape.py
@@ -1,30 +1,31 @@
 import netCDF4 as nc
 from builder import climate_db
+from psycopg2.extras import execute_values
+import numpy
+import geojson
 
 def print_info(path):
     ds = nc.Dataset(path)
     print(ds)
 
-def import_grid(db,fn):        
+def load_grid(db,fn):        
     features = []
    
     ds = nc.Dataset(fn)
-    print(ds.variables['latitude'])
+    print(ds.variables)
     print(ds.dimensions)
     y_size = ds.dimensions['y'].size
     x_size = ds.dimensions['x'].size
     features = []
-    n = 0
-    for x in range(0,x_size):
-        s = ""
-        for y in range(0,y_size):
+    for x in range(0,x_size-1):
+        for y in range(0,y_size-1):
+            print(ds['x'][y][x])
             features.append(geojson.Feature(id=x*y_size+y, geometry=geojson.Polygon([[
-                (ds['lon'][x][y],ds['lat'][x][y]),
-                (ds['lon'][x+1][y],ds['lat'][x+1][y]),
-                (ds['lon'][x+1][y+1],ds['lat'][x+1][y+1]),
-                (ds['lon'][x][y+1],ds['late'][x][y+1])]]),
-                                            properties={}))
-            n+=1
+                (float(ds['x'][y][x]),float(ds['y'][y][x])),
+                (float(ds['x'][y+1][x]),float(ds['y'][y+1][x])),
+                (float(ds['x'][y+1][x+1]),float(ds['y'][y+1][x+1])),
+                (float(ds['x'][y][x+1]),float(ds['y'][y][x+1]))]])))
+        print((x*x_size+y)/(x_size*y_size))
             
     db.import_geojson_feature("chessscape_grid","27700",geojson.FeatureCollection(features))
     db.conn.commit()
@@ -44,23 +45,35 @@ def load_data(db,fn,table,variable):
     time_size = ds.dimensions['time'].size # P3M = 3 months?
     y_size = ds.dimensions['y'].size
     x_size = ds.dimensions['x'].size
-
     for t in range(0,time_size):
         print("t: "+str(t))        
-        for x in range(0,x_size):
-            s = []
-            for y in range(0,y_size):
+        for y in range(0,y_size):            
+            values = ds[variable][t][y]
+            dat = []
+            for x in range(0,x_size):
+                value = values[x]
                 grid_id = x*y_size+y
-                value = ds[variable][t][y][x]
-                if type(value) == float:
-                    q=f"insert into {table} (location,season,median) values ({grid_id},{t},{value});"               
-                    db.cur.execute(q)
-            print("x: "+str(x))
+                if value is not numpy.ma.masked:
+                    # convert from kelvin
+                    if variable=="tas": value-=273.15
+                    dat.append([grid_id,t,float(value)])
+            q=f"insert into {table} (location,season,value) values %s"
+            execute_values(db.cur,q,dat)
         print("committing")
         db.conn.commit()
 
-def import_data(db):
+# wget -r -nH --cut-dirs=5 -nc "ftp://username:password@ftp.ceda.ac.uk//badc/deposited2021/chess-scape/data/rcp85_bias-corrected/01/seasonal"
+
+def import_grid(db):
+    variable = "tas"
     path = "/home/dave/projects/climate/v2-data/chess/rcp85_bias-corrected/01/seasonal/"
-    fn = "chess-scape_rcp85_bias-corrected_01_hurs_uk_1km_seasonal_19801201-20801130.nc"
+    fn = "chess-scape_rcp85_bias-corrected_01_"+variable+"_uk_1km_seasonal_19801201-20801130.nc"
     print_info(path+fn)
-    load_data(db,path+fn,"chessscape_seasonal_hurs","hurs")
+    load_grid(db,path+fn)
+        
+def import_data(db):
+    variable = "tas"
+    path = "/home/dave/projects/climate/v2-data/chess/rcp85_bias-corrected/01/seasonal/"
+    fn = "chess-scape_rcp85_bias-corrected_01_"+variable+"_uk_1km_seasonal_19801201-20801130.nc"
+    print_info(path+fn)
+    load_data(db,path+fn,"chessscape_seasonal_"+variable,variable)
