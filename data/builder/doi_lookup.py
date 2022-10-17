b2daf21cda344078ffbbd3caffe67f2ca4f903ca
Dave Griffiths <dave@thentrythis.org>
2022-10-17 14:14:53 +0100

    updated data python requirements

--- data/builder/doi_lookup.py
+++ data/builder/doi_lookup.py
@@ -8,11 +8,7 @@ import time
 class doi_lookup:
     def __init__(self):
         self.doicache = {}
-
-        self.link_overrides = {
-            "10.1016/j.scitotenv.2020.136678":"https://www.researchgate.net/publication/338601800_Suburb-level_changes_for_active_transport_to_meet_the_SDGs_Causal_theory_and_a_New_Zealand_case_study",
-            "10.1016/j.jtrangeo.2019.04.016":"https://www.researchgate.net/publication/332932885_Weather_and_cycling_in_New_York_The_case_of_Citibike"    
-        }
+        self.link_overrides = {}
 
 
     def print():
@@ -26,72 +22,102 @@ class doi_lookup:
     # elements:
     # Label, Type, Tags, Description, References, DOI, UN SDG, Variables
     # Tags = "climate variable"
-    def doi2info(self,doi):
+    def doi2info(self,doi,type):
         if doi in self.doicache:
             return self.doicache[doi]
         
-        if doi.startswith("http") or doi.startswith("Reference"):
-            self.doicache[doi] = { 
-                "doi": doi,
-                "type": "link",
-                "link": doi
-            }
-            return self.doicache[doi]
-    
         opener = urllib.request.build_opener()
         opener.addheaders = [('Accept', 'application/vnd.crossref.unixsd+xml')]
         while True:
             print("reading doi.org: "+doi)
             r = opener.open('http://dx.doi.org/'+doi)
             d = xmltodict.parse(r.read())
-            print("done")
-            print (json.dumps(d["crossref_result"]["query_result"]["body"]["query"]["doi_record"], indent=4))
 
-            journal = d["crossref_result"]["query_result"]["body"]["query"]["doi_record"]["crossref"]["journal"]
-            article = journal["journal_article"]
-
-            #print(d["crossref_result"]["query_result"]["body"]["query"]["doi_record"]["crossref"]["journal"]["journal_article"])
-            title = article["titles"]["title"]
-
-            authors = ""
-            for contributor in article["contributors"]["person_name"]:
-                if contributor["@contributor_role"]=="author":
-                    authors+=(contributor["given_name"]+" "+contributor["surname"]+", ")
-
-            date = ""
-            if isinstance(article["publication_date"], list):
-                for d in article["publication_date"]:
-                    if d["@media_type"]=="print":
-                        date = d["year"]
+            print(type)
+            if type=="Journal Article":
+                self.read_article(doi,d)
             else:
-                date=article["publication_date"]["year"]
-                
-            journal_title = journal["journal_metadata"]["full_title"]
-            #print(journal["journal_issue"])
-
-            issue=""
-            if "journal_issue" in journal:
-                if "issue" in journal["journal_issue"]:
-                    issue = journal["journal_issue"]["issue"]
-
-            link = "http://doi.org/"+doi
-
-            if doi in self.link_overrides:
-                link = self.link_overrides[doi]
-                    
-            self.doicache[doi]={
-                "type": "article",
-                "doi": doi,
-                "title": title,
-                "authors": authors,
-                "date": date,
-                "journal": journal_title,
-                "issue": issue,
-                "link": link
-            }
+                self.read_book(doi,d)
 
             return self.doicache[doi]
 
+
+    def read_book(self,doi,d):
+        #print (json.dumps(d["crossref_result"]["query_result"]["body"]["query"]["doi_record"], indent=4))
+
+        #print (json.dumps(d["crossref_result"]["query_result"]["body"]["query"]["doi_record"], indent=4))
+        book = d["crossref_result"]["query_result"]["body"]["query"]["doi_record"]["crossref"]["book"]
+
+        #print(d["crossref_result"]["query_result"]["body"]["query"]["doi_record"]["crossref"]["journal"]["journal_article"])
+        title = book["book_metadata"]["titles"]["title"]
+        
+        authors = ""
+        for contributor in book["content_item"]["contributors"]["person_name"]:
+            authors+=(contributor["given_name"]+" "+contributor["surname"]+", ")
+                
+        
+        date=book["book_metadata"]["publication_date"]["year"]
+                
+        journal_title = ""
+
+        issue=""
+
+        self.doicache[doi]={
+            "type": "article",
+            "doi": doi,
+            "title": title,
+            "authors": authors,
+            "date": date,
+            "journal": journal_title,
+            "issue": issue,
+        }
+
+    def read_article(self,doi,d):
+        journal = d["crossref_result"]["query_result"]["body"]["query"]["doi_record"]["crossref"]["journal"]
+        article = journal["journal_article"]
+
+        #print(d["crossref_result"]["query_result"]["body"]["query"]["doi_record"]["crossref"]["journal"]["journal_article"])
+        title = article["titles"]["title"]
+        
+        authors = ""
+        print(article["contributors"])
+        for contributor in article["contributors"]:
+            print(contributor)
+            if contributor["@contributor_role"]=="author":
+                authors+=(contributor["given_name"]+" "+contributor["surname"]+", ")
+                
+        date = ""
+        if isinstance(article["publication_date"], list):
+            for d in article["publication_date"]:
+                if d["@media_type"]=="print":
+                    date = d["year"]
+        else:
+            date=article["publication_date"]["year"]
+                
+        journal_title = journal["journal_metadata"]["full_title"]
+        #print(journal["journal_issue"])
+
+        issue=""
+        if "journal_issue" in journal:
+            if "issue" in journal["journal_issue"]:
+                issue = journal["journal_issue"]["issue"]
+
+        link = "http://doi.org/"+doi
+
+        if doi in self.link_overrides:
+            link = self.link_overrides[doi]
+                    
+        self.doicache[doi]={
+            "type": "article",
+            "doi": doi,
+            "title": title,
+            "authors": authors,
+            "date": date,
+            "journal": journal_title,
+            "issue": issue,
+        }
+
+        
     def get_reference(self,doi):
         if doi in self.doicache:
             return self.doicache[doi]
--- data/builder/network_from_json.py
+++ data/builder/network_from_json.py
@@ -29,8 +29,9 @@ def sg(el,key):
 class network_db:
     def __init__(self,db):
         self.db = db
+        self.doi_lookup = doi_lookup.doi_lookup()
 
-    def reset(self):
+    def reset_network(self):
         self.db.cur.execute("drop table if exists network_nodes cascade")
         q = """create table network_nodes (node_id text primary key,
         label text,
@@ -54,9 +55,10 @@ class network_db:
         constraint fk_from foreign key(node_from) references network_nodes(node_id),  
         constraint fk_to foreign key(node_to) references network_nodes(node_id));"""
         self.db.cur.execute(q)
-        
+
+    def reset_refs(self):        
         self.db.cur.execute("drop table if exists articles cascade")
-        q = """create table articles (article_id serial primary key, 
+        q = """create table articles (article_id int primary key, 
         doi text,
         type text,
         link text,                            
@@ -64,7 +66,8 @@ class network_db:
         authors text,
         date text,
         journal text,
-        issue text)"""
+        issue text,
+        notes text)"""
         self.db.cur.execute(q)
         
         self.db.cur.execute("drop table if exists node_article_mapping cascade")
@@ -114,32 +117,36 @@ class network_db:
         self.db.conn.commit()
         return edge['_id']
                     
-    def add_ref(self,ref):
+    def add_ref(self,row):
         print("add_article")
-        print(ref)
-        ## check if it exists already
-        self.db.cur.execute(f"select article_id from articles where doi='{ref['doi']}'")
-        res = self.db.cur.fetchall()
-        if len(res)!=0:
-            return res[0][0]
-        
-        if ref['type']=="article":
-            self.db.cur.execute(f"""insert into articles (doi, type, link, title, authors, date, journal, issue) values
-            ('{ref["doi"]}',
-            '{ref["type"]}',
-            '{ref["link"]}',
-            '{ref["title"]}',
-            '{ref["authors"]}',
-            '{ref["date"]}',
-            '{ref["journal"]}',
-            '{ref["issue"]}') returning article_id""")        
+
+        ref = False
+        if row[2]!="":
+            ref = self.doi_lookup.doi2info(row[2],row[1])
+
+        print(row)
+            
+        if ref!=False:
+            self.db.cur.execute("insert into articles (article_id, doi, type, link, title, authors, date, journal, issue, notes) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+            (row[0],
+             ref["doi"],
+             ref["type"],
+             row[3],
+             ref["title"],
+             ref["authors"],
+             ref["date"],
+             ref["journal"],
+             ref["issue"],
+             row[5]))        
         else:
-            self.db.cur.execute(f"""insert into articles (doi, type, link) values
-            ('{ref["doi"]}',
-            '{ref["type"]}',
-            '{ref["link"]}') returning article_id""")
+            self.db.cur.execute("insert into articles (article_id, doi, type, link, notes) values (%s, %s, %s, %s, %s)",
+            (row[0],
+             row[2],
+             row[1],
+             row[3],
+             row[5]))
         self.db.conn.commit()
-        return self.db.cur.fetchone()[0]
+        return row[0]
 
     def add_node_article_mapping(self, node_id, article_id):
         self.db.cur.execute(f"""insert into node_article_mapping (node_id, article_id) values
@@ -154,19 +161,15 @@ class network_db:
         return self.db.cur.fetchone()[0]
             
 
-def reset(db):
-    nd = network_db(db)
-    nd.reset()
-
 def find_item(l,id):
     for el in l:
         if el["_id"]==id: return el
     print(id+" not found")
     return False
     
-def load(db,path,mapname):
-    reset(db)
+def load(db,path,mapname):    
     nd = network_db(db)
+    nd.reset_network()
     
     net = json.load(open(path))
 
@@ -177,9 +180,16 @@ def load(db,path,mapname):
             for edge in map["connections"]:
                 nd.add_edge(find_item(net["connections"],edge["connection"]))
 
-    #for ref in np.refs: nd.add_ref(ref)
-    
-
+def load_refs(db,refssheet):                
+    nd = network_db(db)
+    nd.reset_refs()
+ 
+    with open(refssheet) as csvfile:
+        reader = csv.reader(csvfile)
+        for i,row in enumerate(reader):
+            if i>0:
+                nd.add_ref(row)
+                
  
 
                                    
