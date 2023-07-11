f0cf7de49f29d2d826744dee66f05a3183819e46
Dave Griffiths <dave@thentrythis.org>
2023-07-11 17:04:06 +0100

    upgraded the server to es6 and running

--- server/app.js
+++ server/app.js
@@ -13,14 +13,18 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var admin = require('./admin');
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import apiRouter from './routes/api.js';
+//import admin from './admin.js';
 
-var apiRouter = require('./routes/api');
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 var app = express();
 
@@ -45,7 +49,7 @@ app.use(function(req, res, next) {
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', apiRouter);
 
-admin.setup(app);
+//admin.setup(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -63,5 +67,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-module.exports = app;
+export default app;
