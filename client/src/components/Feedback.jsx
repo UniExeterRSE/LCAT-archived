e102c68b215d499619bb91f6be788eda4b7924d0
Simon Kirby <s.kirby@exeter.ac.uk>
2024-01-31 15:09:07 +0000

    Create Feedback component.

---
+++ client/src/components/Feedback.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Feedback = () => {
+    return (
+        <div className="feedback">
+            <h1>Feedback</h1>
+            <p>
+                LCAT has been designed alongside local decision-makers, whose feedback has been key in the tool's
+                development. However, we are always looking for feedback on the functionality and usability of LCAT.
+                Please find a link to our evaluation questionaire here.
+            </p>
+            <p>
+                <a
+                    // className="""
+                    href=""
+                    target="_blank"
+                    rel="noreferrer"
+                ></a>
+            </p>
+        </div>
+    );
+};
+
+export default Feedback;
