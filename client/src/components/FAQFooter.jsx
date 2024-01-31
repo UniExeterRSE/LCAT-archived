1a29e006aa1f9801f848b6b1cf32e1a0ee6cda70
Simon Kirby <s.kirby@exeter.ac.uk>
2024-01-31 12:55:44 +0000

    Add FAQFooter component.

---
+++ client/src/components/FAQFooter.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const FAQFooter = () => {
+    return (
+        <div className="footer-flex-container">
+            <div class="footer-heading">Frequently Asked Questions</div>
+            <div class="footer-flex-content">
+                <p>
+                    <a
+                        class="faq-button"
+                        href="https://www.ecehh.org/wp/wp-content/uploads/2023/01/Frequently-Asked-Questions-1.pdf"
+                    ></a>
+                </p>
+                <p>
+                    View&nbsp;
+                    <a href="https://www.ecehh.org/wp/wp-content/uploads/2023/01/Frequently-Asked-Questions-1.pdf">
+                        Frequently Asked Questions at: ecehh.org
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default FAQFooter;
