08c643dcd14bcc640d26941aaadceadf60456a84
Simon Kirby <s.kirby@exeter.ac.uk>
2024-02-09 14:35:44 +0000

    Add all Kumu embed links to project.

--- client/src/components/ClimateImpactSummaryData.js
+++ client/src/components/ClimateImpactSummaryData.js
@@ -61,26 +61,74 @@ export const pathways = [
     {
         id: 1,
         name: "Coastal Security",
-        summaryPathwayMap: "",
-        completePathwayMap: "",
+        summaryPathwayMap: (
+            <iframe
+                title="Summary Pathway Map for Coastal Security"
+                src="https://embed.kumu.io/dcfcc101c8680fee0f43e7b912cdeddf"
+                frameborder="0"
+            />
+        ),
+        completePathwayMap: (
+            <iframe
+                title="Complete Pathway Map for Coastal Security"
+                src="https://embed.kumu.io/77450eb423d7c26a2ca9eccfd0294576"
+                frameborder="0"
+            />
+        ),
     },
     {
         id: 2,
         name: "Flooding and Drought",
-        summaryPathwayMap: "",
-        completePathwayMap: "",
+        summaryPathwayMap: (
+            <iframe
+                title="Summary Pathway Map for Flooding and Drought"
+                src="https://embed.kumu.io/83dc6a68ceff92c0dc124c984d4f19a4"
+                frameborder="0"
+            />
+        ),
+        completePathwayMap: (
+            <iframe
+                title="Complete Pathway Map for Flooding and Drought"
+                src="https://embed.kumu.io/fc123acf574fe6e789a6cb6255f7eecc"
+                frameborder="0"
+            />
+        ),
     },
     {
         id: 3,
         name: "Food and Personal Security",
-        summaryPathwayMap: "",
-        completePathwayMap: "",
+        summaryPathwayMap: (
+            <iframe
+                title="Summary Pathway Map for Food and Personal Security"
+                src="https://embed.kumu.io/0af7e916299599d68384abf7a153b63e"
+                frameborder="0"
+            />
+        ),
+        completePathwayMap: (
+            <iframe
+                title="Complete Pathway Map for Food and Personal Security"
+                src="https://embed.kumu.io/97d06affef1371cd23724be3e55f8aba"
+                frameborder="0"
+            />
+        ),
     },
     {
         id: 4,
         name: "Pathogenic Marine Microorganisms",
-        summaryPathwayMap: "",
-        completePathwayMap: "",
+        summaryPathwayMap: (
+            <iframe
+                title="Summary Pathway Map for Pathogenic Marine Microorganisms"
+                src="https://embed.kumu.io/7d23ed338ab4604f1016142daf7bfd52"
+                frameborder="0"
+            />
+        ),
+        completePathwayMap: (
+            <iframe
+                title="Complete Pathway Map for Pathogenic Marine Microorganisms"
+                src="https://embed.kumu.io/b6e535790518a1259bc45bcb6880350e"
+                frameborder="0"
+            />
+        ),
     },
     {
         id: 5,
