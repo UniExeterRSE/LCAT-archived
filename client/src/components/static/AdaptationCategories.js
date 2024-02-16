df155761a56460ca47e5b9f6420428e65cb6e7d9
Simon Kirby <s.kirby@exeter.ac.uk>
2024-02-16 14:30:40 +0000

    Add adaptation themes and categories.

---
+++ client/src/components/static/AdaptationCategories.js
@@ -0,0 +1,29 @@
+export const CCCAdaptationThemes = [
+    "Nature",
+    "Working lands and seas",
+    "Towns and cities",
+    "Community preparedness and response",
+    "Health",
+    "Energy",
+    "Transport",
+    "Buildings",
+    "Food security",
+    "Telecoms & ICT",
+    "Business",
+    "Finance",
+    "Water supply",
+    "All",
+];
+
+export const IPCCCategories = [
+    "Engineered and built environment",
+    "Educational",
+    "Technological ",
+    "Ecosystem-based",
+    "Services",
+    "Laws and regulations",
+    "Government policies and programs",
+    "Economic",
+    "Behavioural",
+    "Informational",
+];
